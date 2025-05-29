c800609fc9f560f72d48f1256f0335fe5b6511c5
G S Shashank yadav <gsshashankyadav@gmail.com>
2025-05-29 22:36:10 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Productivity Dashboard',
+  description: 'Productivity Dashboard To Improve your Productivity'
 }
 
 export default function RootLayout({
